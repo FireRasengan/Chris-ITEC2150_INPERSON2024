12ce552d68b44561fb172a9203146f03b38b7175
cjabbour <jabbourcmj@gmail.com>
2024-02-12 11:06:22 -0500

    all committed

--- src/inheritanceDemoV3/Mammal.java
+++ src/inheritanceDemoV3/Mammal.java
@@ -6,47 +6,47 @@ public class Mammal extends Animal {
     private boolean giveBirth;
 
     public Mammal(){
-        super(false,2, "Omnivore");
+        //super(false,2, "Omnivore");
     }
 
     public Mammal(boolean hasFur, boolean giveBirth){
-        super(false, 2, "Carnivore");
+        //super(false, 2, "Carnivore");
         this.hasFur = hasFur;
         this.giveBirth = giveBirth;
         //this();
     }
 
     public Mammal(boolean beingDangerous, int numberOfLegs, String diet, boolean hasFur, boolean giveBirth){
-        super(beingDangerous, numberOfLegs, diet);
+        //super(beingDangerous, numberOfLegs, diet);
         this.hasFur = hasFur;
         this.giveBirth = giveBirth;
     }
 
     public Mammal(Animal animal){
-        super(animal.beingDangerous,animal.numberOfLegs, animal.diet);
+        //super(animal.beingDangerous,animal.numberOfLegs, animal.diet);
     }
 
     public Mammal(Animal animal, boolean hasFur, boolean giveBirth){
-        super(animal.beingDangerous,animal.numberOfLegs, animal.diet);
+        //super(animal.beingDangerous,animal.numberOfLegs, animal.diet);
         this.hasFur = hasFur;
         this.giveBirth = giveBirth;
     }
 
     public Mammal(String name, boolean hasFur, boolean giveBirth){
-        super();
+        //super();
         this.hasFur = hasFur;
         this.giveBirth = giveBirth;
     }
 
     @Override
     public void makeNoise(){
-        super.makeNoise();
+        //super.makeNoise();
         System.out.println(" Meow~~~");
     }
 
     @Override
     public void eat(){
-        super.eat();
+        //super.eat();
         System.out.println(" Yummy ");
     }
 
