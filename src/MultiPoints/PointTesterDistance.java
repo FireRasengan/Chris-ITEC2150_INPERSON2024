a50f09cecabecba50e5cbce8a3375e65829d574f
cjabbour <jabbourcmj@gmail.com>
2024-02-14 12:12:05 -0500

    all committed

--- src/MultiPoints/PointTesterDistance.java
+++ src/MultiPoints/PointTesterDistance.java
@@ -23,6 +23,7 @@ public class PointTesterDistance {
 
     public static void distance(Point p1, Point p2) {
         double distanceFinal = Point.distance(p1, p2);
-        System.out.printf("The distance between (%.2f , %.2f) and (%.2f , %.2f) is %.2f", p1.getX(), p1.getY(), p2.getX(), p2.getY(), distanceFinal);
+        System.out.printf("The distance between (%.2f , %.2f) and (%.2f , %.2f) is %.2f",
+                p1.getX(), p1.getY(), p2.getX(), p2.getY(), distanceFinal);
     }
 }
---
+++ src/MultiPoints/SixPointTester.java
@@ -0,0 +1,43 @@
+package MultiPoints;
+
+/**
+ * class: SixPointTester
+ * description
+ * @author: cjabbour
+ */
+import java.util.Scanner;
+public class SixPointTester {
+    public static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        computeShortestDistance(enterPoints());
+    }
+
+    public static Point[] enterPoints() {
+        Point[] points = new Point[6];
+        System.out.println("Enter 6 points of x and y coordinates: ");
+
+        for (int i = 0; i <= points.length; i++) {
+            points[i] = new Point(input.nextDouble(), input.nextDouble());
+        }
+        return points;
+    }
+
+    public static void computeShortestDistance(Point[] points) {
+        double shortestDistance = Double.MAX_VALUE;
+        Point p1 = null;
+        Point p2 = null;
+
+        for (int i = 0; i < points.length; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+                double dist = Point.distance(points[i], points[j]);
+                if (dist < shortestDistance) {
+                    shortestDistance = dist;
+                    p1 = points[i];
+                    p2 = points[j];
+                }
+            }
+        }
+        System.out.printf("The shortest distance of two points are (%.1f, %.1f) and (%.1f, %.1f) " +
+                "and their distance is (%.2f)", p1.getX(), p1.getY(), p2.getX(), p2.getY(), shortestDistance);
+    }
+}
