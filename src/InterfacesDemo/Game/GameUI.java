7a468e9c7a8fc9ece221c2b183e86bdfefd657e5
cjabbour <jabbourcmj@gmail.com>
2024-03-15 12:14:18 -0400

    all committed

--- src/InterfacesDemo/Game/GameUI.java
+++ src/InterfacesDemo/Game/GameUI.java
@@ -8,6 +8,9 @@ public class GameUI {
         ISavable k1 = new King("Henry the King", 2, "one at a time in any direction", "Black");
         ISavable q1 = new Queen("Elizabeth the Queen", 6, "any direction as far", "White");
 
+        saveObject(k1);
+        saveObject(q1);
+
     }
 
     public static void saveObject(ISavable isobj) {
@@ -19,10 +22,26 @@ public class GameUI {
     public static ArrayList<String> readValues() {
         ArrayList<String> values = new ArrayList<String>();
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Choose 1 to enter a string or 0 to quit");
+        int choice = scanner.nextInt();
+        int index = 0;
+        while (choice != 0) {
+            switch (choice) {
+                case 0:
+                    break;
+                case 1:
+                    System.out.println("Enter a string:");
+                    String strInput = scanner.nextLine();
+                    values.add(index, strInput);
+                    index++;
+                    break;
+            }
+        }
+        return values;
     }
 
     public static void loading(ISavable isobj) {
-
+        ArrayList<String> aList = readValues();
+        isobj.read(aList);
     }
 }
--- src/InterfacesDemo/Game/ISavable.java
+++ src/InterfacesDemo/Game/ISavable.java
@@ -4,6 +4,4 @@ import java.util.List;
 public interface ISavable {
     List<String> write();
     void read(List<String> listOfSavedValues);
-
-
 }
